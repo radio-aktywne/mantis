7697d565ad27e96da098ab090301f92b0c856317
Sebastian Pietras <spietras1998@gmail.com>
2022-11-04 13:37:42 +0100

    Added proper exception when task not found (#19)

--- emischeduler/src/emischeduler/api/paths/tasks/controller.py
+++ emischeduler/src/emischeduler/api/paths/tasks/controller.py
@@ -1,6 +1,6 @@
 from rocketry import Rocketry
 from rocketry.core import Task as RocketryTask
-from starlite import Controller, State, get, post
+from starlite import Controller, State, get, post, NotFoundException
 
 from emischeduler.api.models import Task
 from emischeduler.api.paths.tasks.models import TasksResponse, TaskResponse
@@ -15,38 +15,48 @@ class TasksController(Controller):
         tasks = [Task.from_task(task) for task in scheduler.session.tasks]
         return TasksResponse(tasks=tasks)
 
-    @get("/{task_name:str}")
+    @get("/{task_name:str}", raises=[NotFoundException])
     async def task(self, state: State, task_name: str) -> TaskResponse:
         scheduler: Rocketry = state.scheduler
+        if task_name not in scheduler.session:
+            raise NotFoundException(f"Task {task_name} not found.")
         task: RocketryTask = scheduler.session[task_name]
         return TaskResponse(task=Task.from_task(task))
 
-    @post("/{task_name:str}/disable")
+    @post("/{task_name:str}/disable", raises=[NotFoundException])
     async def disable_task(self, state: State, task_name: str) -> TaskResponse:
         scheduler: Rocketry = state.scheduler
+        if task_name not in scheduler.session:
+            raise NotFoundException(f"Task {task_name} not found.")
         task: RocketryTask = scheduler.session[task_name]
         task.disabled = True
         return TaskResponse(task=Task.from_task(task))
 
-    @post("/{task_name:str}/enable")
+    @post("/{task_name:str}/enable", raises=[NotFoundException])
     async def enable_task(self, state: State, task_name: str) -> TaskResponse:
         scheduler: Rocketry = state.scheduler
+        if task_name not in scheduler.session:
+            raise NotFoundException(f"Task {task_name} not found.")
         task: RocketryTask = scheduler.session[task_name]
         task.disabled = False
         return TaskResponse(task=Task.from_task(task))
 
-    @post("/{task_name:str}/terminate")
+    @post("/{task_name:str}/terminate", raises=[NotFoundException])
     async def terminate_task(
         self, state: State, task_name: str
     ) -> TaskResponse:
         scheduler: Rocketry = state.scheduler
+        if task_name not in scheduler.session:
+            raise NotFoundException(f"Task {task_name} not found.")
         task: RocketryTask = scheduler.session[task_name]
         task.force_termination = True
         return TaskResponse(task=Task.from_task(task))
 
-    @post("/{task_name:str}/run")
+    @post("/{task_name:str}/run", raises=[NotFoundException])
     async def run_task(self, state: State, task_name: str) -> TaskResponse:
         scheduler: Rocketry = state.scheduler
+        if task_name not in scheduler.session:
+            raise NotFoundException(f"Task {task_name} not found.")
         task: RocketryTask = scheduler.session[task_name]
         task.force_run = True
         return TaskResponse(task=Task.from_task(task))
